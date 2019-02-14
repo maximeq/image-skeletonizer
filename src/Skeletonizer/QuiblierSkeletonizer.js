f967eef4b6eb29f2f854350c9034b3617bf82da0
Maxime Quiblier <maximequiblier@gmail.com>
2019-02-14 23:06:40 +0100

    Some new tests on the experimental skeletonizer

--- src/Skeletonizer/QuiblierSkeletonizer.js
+++ src/Skeletonizer/QuiblierSkeletonizer.js
@@ -15,31 +15,57 @@ QuiblierSkeletonizer.prototype.buildHierarchy = function(){
     const height = this.distImg.height;
     const size = width*height;
 
+    // Accepted error in pixels
+    var threshold = 3;
+
+    var nodes_set = {};
+    var nodes = [];
+
     var covered = new Array(size);
     for(var i=0; i<covered.length;++i){
         covered[i] = false;
     }
 
-    var addCover = function(node){
-        var cx =node.position.x;
-        var cy = node.position.y;
-        var w = Math.ceil(node.weight);
-        for(var x=node.position.x-w; x<node.position.x+w; x++){
-            for(var y=node.position.y-w; y<node.position.y+w; y++){
-                if((x-cx)*(x-cx)+(y-cy)*(y-cy) <= w*w){
-                    var idx = y*width+x;
-                    covered[idx] = true;
+    // Add the zone covered by node and update the candidate list
+    var self = this;
+    var addCover = function(nodeA, nodeB, candidates){
+
+        if(nodeB === null){ // only one disk to cover
+            var cx =nodeA.position.x;
+            var cy = nodeA.position.y;
+            var w = Math.ceil(nodeA.weight);
+            for(var x=nodeA.position.x-w; x<nodeA.position.x+w; x++){
+                for(var y=nodeA.position.y-w; y<nodeA.position.y+w; y++){
+                    var dist_sq = (x-cx)*(x-cx)+(y-cy)*(y-cy)
+                    if(dist_sq <= w*w){
+                        var idx = y*width+x;
+                        covered[idx] = true;
+                        delete candidates[idx];
+                    }
+                }
+            }
+            // fill in new candidates
+            var circumf = Math.PI*2*(nodeA.weight+1);
+            var l = Math.round(circumf);
+            for(var i=0; i<l; ++i){
+                var angle = i*2*Math.PI/l;
+                var x = Math.round(nodeA.position.x + Math.cos(angle)*(nodeA.weight+1));
+                var y = Math.round(nodeA.position.y + Math.sin(angle)*(nodeA.weight+1));
+                var idx = y*width+x;
+                var v = self.distImg.getValue(x,y)/self.distImg.getCoeff();
+                if(!covered[idx] && v > threshold){
+                    candidates[idx] = true;
                 }
             }
         }
     };
 
-    // find the point with highes distance
+    // find the point with highest distance
     var max = 0;
     var max_x = -1;
     var max_y = -1;
-    for(var x=0; x<this.distImg.width; ++x){
-        for(var y=0; y<this.distImg.height; ++y){
+    for(var x=0; x<width; ++x){
+        for(var y=0; y<height; ++y){
             var v = this.distImg.getValue(x,y);
             if(v > max){
                 max = v;
@@ -49,100 +75,36 @@ QuiblierSkeletonizer.prototype.buildHierarchy = function(){
         }
     }
 
-    var first_node = new SkeletonNode(new Point2D(max_x,max_y),max/this.distImg.getCoeff(), new Map());
-    addCover(first_node);
+    var first_node = new SkeletonNode(new Point2D(max_x,max_y),max/this.distImg.getCoeff());
+    nodes_set[this.distImg.getIndex(max_x,max_y)] = first_node;
+    nodes.push(first_node);
+    var candidates = {};
+    addCover(first_node, null, candidates);
 
-    var self = this;
-    var exploreNode = function(node, dist_img){
-        var circumf = Math.PI*2*node.weight;
-        var l = Math.round(circumf);
-        var values = new Array(l);
-        var xs     = new Array(l);
-        var ys     = new Array(l);
-
-        // Build the countour points, 1 pixel outside the current node.
-        for(var i=0; i<l; ++i){
-            var angle = i*2*Math.PI/l;
-            var x = Math.round(node.position.x + Math.cos(angle)*(node.weight+1));
-            var y = Math.round(node.position.y + Math.sin(angle)*(node.weight+1));
-            values[i] = self.distImg.getValue(x,y)/self.distImg.getCoeff();
-            xs[i] = x;
-            ys[i] = y;
-        }
-
-        // find the maximum on boundaries, which is not yet covered.
-        var max_v = -1;
+    var ck = Object.keys(candidates);
+    while(ck.length !== 0){
         var max_i = -1;
-        for(var i=0; i<values.length; ++i){
-            var idx = ys[i]*width+xs[i];
-            if(!covered[idx]){
-                if(values[i]>max_v){
-                    max_v = values[i];
-                    max_i = i;
-                }
+        var max_v = 0;
+        for(var i=0; i<ck.length; ++i){
+            var idx = parseInt(ck[i]);
+            if(covered[idx]){
+                delete candidates[ck[i]];
+            }else if(this.distImg.getIndexValue(idx)>max_v){
+                max_v = this.distImg.getIndexValue(idx);
+                max_i = idx;
             }
         }
 
-        if(max_v > 2){
-            var new_node = new SkeletonNode(new Point2D(xs[max_i],ys[max_i]),max_v, new Map());
-            node.getNeighbors().set(xs[max_i]+";"+ys[max_i], new_node);
-            addCover(new_node);
-            return new_node;
-        }else{
-            return null;
-        }
-    };
+        var new_node = new SkeletonNode(new Point2D(this.distImg.getXFromIndex(max_i),this.distImg.getYFromIndex(max_i)),max_v/this.distImg.getCoeff());
+        nodes_set[max_i] = new_node;
+        nodes.push(new_node);
+        delete candidates[max_i];
+        addCover(new_node, null, candidates);
 
-    var exp_n = first_node;
-    var counter = 1;
-    while(exp_n !== null){
-        exp_n = exploreNode(exp_n);
-        counter++;
+        ck = Object.keys(candidates);
     }
 
-    return first_node;
-};
-
-/**
- *  @param {SkeletonNode} h A hierarchy built with buildHierarchy.
- */
-QuiblierSkeletonizer.prototype.getHierarchyInImageData = function(h){
-    var res = this.distImg.getImageData();
-
-    var nodes = [];
-
-    var recFindAllNodes = function(node){
-        nodes.push(node);
-        node.getNeighbors().forEach(function(value, key, map) {
-            recFindAllNodes(value);
-        });
-    };
-    recFindAllNodes(h);
-
-    for(var x=0; x<res.width; ++x){
-        for(var y=0; y<res.height; ++y){
-            var avg_n = 0;
-            for(var i=0; i<nodes.length; ++i){
-                var cx = nodes[i].position.x+0.5;
-                var cy = nodes[i].position.y+0.5;
-                if((x+0.5-cx)*(x+0.5-cx)+(y+0.5-cy)*(y+0.5-cy) <= nodes[i].weight*nodes[i].weight){
-                    avg_n++;
-                }
-            }
-            var idx = 4*(y*res.width+x);
-            res.data[idx+1] = (res.data[idx]+avg_n*255)/(avg_n+1);
-        }
-    }
-
-    for(var i=0; i<nodes.length; ++i){
-        var n = nodes[i];
-        var idx = 4*(n.position.y*res.width+n.position.x);
-        res.data[idx] = 255;
-        res.data[idx+1] = 0;
-        res.data[idx+2] = 0;
-    }
-
-    return res;
+    return nodes;
 };
 
 module.exports = QuiblierSkeletonizer;
